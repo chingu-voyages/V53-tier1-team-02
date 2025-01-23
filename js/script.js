995ac01cddc1554cbacb26b8124cdbaa63058a78
ZohebTahir <zohebt.dev@gmail.com>
2025-01-23 02:31:01 -0800

    Sched Modal 1.0

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const scheduleBtn = document.getElementById("schedule-button");
+const employeeBtn = document.getElementById("employee-button");
+const closeModalBtn = document.querySelector(".close-button");
+const overlay = document.getElementById("overlay");
+const scheduleModalDiv = document.getElementById("schedule-modal");
+const generateMenuBtn = document.getElementById("menu-btn");
+
+
+// 1.) Schedule Modal Functionality 
+scheduleBtn.addEventListener("click", () => {
+    const modal = document.querySelector(".modal");
+    openModal(modal);
+})
+
+
+closeModalBtn.addEventListener("click", () => {
+    const modal = document.querySelector(".close-button");
+    closeModal(modal)
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    scheduleModalDiv.classList.add("active");
+    overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    scheduleModalDiv.classList.remove("active");
+    overlay.classList.remove("active");
+}
+
+
+// Generate Menu Button on modal 
+// generateMenuBtn.addEventListener("click", () => {
+    
+// })
