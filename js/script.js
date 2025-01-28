773a22540e260a61c5579e8f67a98a2c32992bc7
Bryan Hoyem <bjhoyem@gmail.com>
2025-01-28 14:01:27 -0600

    Added pseudocode for dev work

--- js/script.js
+++ js/script.js
@@ -152,6 +152,10 @@ function dishPicker(weekday) {
     console.log("randomIndex: " + randomIndex);
     const randomDish = (dishes[randomIndex]);
     console.log(weekday + " dish: " +(randomDish.name));
+    allergycheck(randomDish);
+ }
+
+ function foodDisplay(checkedDish){
     const randomDishIngredients = (randomDish.ingredients).join(", ");
     console.log((randomDishIngredients));
     monDish.textContent = (randomDish.name);
@@ -165,3 +169,15 @@ function dishPicker(weekday) {
 function setDishes() {
     dishPicker("monday");
 }
+
+function allergyCheck(randomDish){
+    if(gluten) {
+        //    does randomDish.ingredients contain wheat||bread||
+        // if the dish fails this check, call selectRandomDish()
+        //  again to get a new dish
+        }
+    if(milk) {
+        // check for milk ingredients
+    }
+}
+
